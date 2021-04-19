02f98496c90d1eefa7b89e274b4b44f08638d0fd
bigDuro <darrylcrockett@gmail.com>
2021-04-18 23:57:50 -0400

    upload is working

--- utilities/ZipFolder.js
+++ utilities/ZipFolder.js
@@ -2,9 +2,9 @@ const path = require('path');
 const zipFolder = require('zip-a-folder');
 const vgdtAPI_zip = 'build/public/api.zip';
 
-const codeigniter4_zip = 'build/non_public/codeigniter4/codeigniter4-alpha.zip';
+const codeigniter4_zip = 'build/non_public/codeigniter4/codeigniter4-beta.zip';
 const publicHtml = 'build/public/vgdt-admin';
-const publicHtml_zip = 'build/public/vgdt-admin-alpha.zip';
+const publicHtml_zip = 'build/public/vgdt-admin-beta.zip';
 const publicAssets = 'src/assets';
 const publicAssets_zip = 'build/public/assets.zip';
 const publicAPI = 'build/public/api';
