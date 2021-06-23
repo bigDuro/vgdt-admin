1831b529c7d72549d930b04f802d8523a6035534
bigDuro <darrylcrockett@gmail.com>
2021-06-22 23:37:30 -0400

    added portal link

--- src/components/CardView/EmployeeCard/index.js
+++ src/components/CardView/EmployeeCard/index.js
@@ -6,7 +6,7 @@ import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
-import { Button } from '@material-ui/core';
+import { Button, Link } from '@material-ui/core';
 import Collapse from '@material-ui/core/Collapse';
 import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
@@ -62,6 +62,11 @@ export default function EmployeeCard(props) {
     setExpanded(!expanded);
   };
 
+  // const handleEmployeeClick = ( position, name ) => {
+  //   if(position === 'driver') {
+  //     window.open('/vgdt-driver/name/');
+  //   }
+  // }
 
   return (
     <Card className={classes.root}>
@@ -80,7 +85,7 @@ export default function EmployeeCard(props) {
                 inputProps={{ 'aria-label': 'primary checkbox' }}
               />
             }
-            title={`${data.firstname} ${data.lastname}`}
+            title={<Link color="primary" href={`/vgdt-${data.position}/name/${data.firstname} ${data.lastname}`}>{data.firstname} {data.lastname}</Link>}
             subheader={`Position: ${data.position}`}
           />
         </Grid>
